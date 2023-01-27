380f31225289920f41ef7bcefe0976125a30d322
KhaledMat <khaledmatloub2001@gmail.com>
2023-01-27 00:07:27 -0500

    createUser function to create a user in the database

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+//TODO: @Mohamed Bedair, add your routers here
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 

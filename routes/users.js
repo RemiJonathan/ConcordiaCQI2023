84d53b81c98b122b8591a198f6b997a93001313a
Remi Jonathan Choquette <contact@remijonathan.com>
2023-01-27 01:46:14 -0500

    Updated user methods

--- routes/users.js
+++ routes/users.js
@@ -2,36 +2,53 @@ const express = require('express');
 const router = express.Router();
 require('dotenv').config();
 
+const User = require('../models/user');
+const {HttpStatusCode} = require("axios");
+
 //const mongoose = require('mongoose');
 
 // Get all users
 router.get('/', (req, res) => {
-    // Code to get all users from the database
-    res.json(users);
+    User.find({}, (err, users) => {
+        if (err) return res.status(500).send(err);
+        res.status(200).json(users);
+    });
 });
 
 // Get a single user
 router.get('/:id', (req, res) => {
-    // Code to get a single user from the database
-    res.json(user);
+    User.findById(req.params.id, (err, user) => {
+        if (err) return res.status(500).send(err);
+        if (!user) return res.status(404).send('User not found');
+        res.status(200).json(user);
+    });
 });
 
 // Create a new user
 router.post('/', (req, res) => {
-    // Code to create a new user in the database
-    res.json({message: 'User created successfully'});
+    const newUser = new User(req.body);
+    newUser.save((err, savedUser) => {
+        if (err) return res.status(500).send(err);
+        res.status(201).json(savedUser);
+    });
 });
 
 // Update a user
 router.put('/:id', (req, res) => {
-    // Code to update a user in the database
-    res.json({message: 'User updated successfully'});
+    User.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedUser) => {
+        if (err) return res.status(500).send(err);
+        if (!updatedUser) return res.status(404).send('User not found');
+        res.status(200).json(updatedUser);
+    });
 });
 
 // Delete a user
 router.delete('/:id', (req, res) => {
-    // Code to delete a user from the database
-    res.json({message: 'User deleted successfully'});
+    User.findOneAndDelete({_id: req.params.id}, (err, user) => {
+        if (err) return res.status(500).json({error: err});
+        if (!user) return res.status(404).json({error: 'User not found'});
+        res.status(HttpStatusCode.Gone)
+    });
 });
 
 
